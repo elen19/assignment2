22ab0a2515ab4cb1355f722211d33c676fb6cd88
Elis <elen19@student.bth.se>
2021-02-23 17:26:51 +0100

    Osäker start

--- servermain.cpp
+++ servermain.cpp
@@ -6,65 +6,118 @@
 
 /* You will to add includes here */
 
-
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <string.h>
+#include <arpa/inet.h>
 // Included to get the support library
-#include <calcLib.h>
+#include "calcLib.h"
 
 #include "protocol.h"
 
-
 using namespace std;
 /* Needs to be global, to be rechable by callback and main */
-int loopCount=0;
-int terminate=0;
-
+int loopCount = 0;
+int terminate = 0;
 
 /* Call back function, will be called when the SIGALRM is raised when the timer expires. */
-void checkJobbList(int signum){
+void checkJobbList(int signum)
+{
   // As anybody can call the handler, its good coding to check the signal number that called it.
 
   printf("Let me be, I want to sleep.\n");
 
-  if(loopCount>20){
+  if (loopCount > 20)
+  {
     printf("I had enough.\n");
-    terminate=1;
+    terminate = 1;
   }
-  
+
   return;
 }
 
+int main(int argc, char *argv[])
+{
 
-
-
-
-int main(int argc, char *argv[]){
-  
   /* Do more magic */
+  if (argc != 2)
+  {
+    printf("Wrong format.\n");
+    exit(0);
+  }
+  char delim[] = ":";
+  char *Desthost = strtok(argv[1], delim);
+  char *Destport = strtok(NULL, delim);
 
+  if (Desthost == NULL || Destport == NULL)
+  {
+    printf("Wrong format\n");
+    exit(0);
+  }
+  int port = atoi(Destport);
+  addrinfo sa, *si, *p;
+  memset(&sa, 0, sizeof(sa));
+  sa.ai_family = AF_INET;
+  sa.ai_socktype = SOCK_DGRAM;
+  sa.ai_protocol = 17;
+  int sockfd;
+  int rv;
+  calcMessage msg;
+  struct timeval tv;
+  tv.tv_sec = 2;
+  tv.tv_usec = 0;
+  if ((rv = getaddrinfo(Desthost, Destport, &sa, &si)) != 0)
+  {
+    fprintf(stderr, "getadrrinfo: %s\n", gai_strerror(rv));
+  }
+  struct sockaddr_in servaddr;
+  for (p = si; p != NULL; p = p->ai_next)
+  {
+    if ((sockfd = socket(p->ai_family, p->ai_socktype, p->ai_protocol)) == -1)
+    {
+      printf("Failed to creat socket.\n");
+      continue;
+    }
+     if ((bind(sockfd, p->ai_addr, p->ai_addrlen)) != 0)
+    {
+      printf("Failed to bind.\n");
+      close(sockfd);
+      continue;
+    }
+    printf("Socket created\n");
+    break;
+  }
+  setsockopt(sockfd, SOL_SOCKET, SO_RCVTIMEO, (const char *)&tv, sizeof(tv));
+  if (p == NULL)
+  {
+    printf("Failed to create socket, p=NULL.\n");
+    freeaddrinfo(si);
+    exit(0);
+  }
 
   /* 
      Prepare to setup a reoccurring event every 10s. If it_interval, or it_value is omitted, it will be a single alarm 10s after it has been set. 
   */
   struct itimerval alarmTime;
-  alarmTime.it_interval.tv_sec=10;
-  alarmTime.it_interval.tv_usec=10;
-  alarmTime.it_value.tv_sec=10;
-  alarmTime.it_value.tv_usec=10;
+  
+  alarmTime.it_interval.tv_sec = 10;
+  alarmTime.it_interval.tv_usec = 10;
+  alarmTime.it_value.tv_sec = 10;
+  alarmTime.it_value.tv_usec = 10;
 
   /* Regiter a callback function, associated with the SIGALRM signal, which will be raised when the alarm goes of */
   signal(SIGALRM, checkJobbList);
-  setitimer(ITIMER_REAL,&alarmTime,NULL); // Start/register the alarm. 
+  setitimer(ITIMER_REAL, &alarmTime, NULL); // Start/register the alarm.
 
-  
-  while(terminate==0){
-    printf("This is the main loop, %d time.\n",loopCount);
+  while (terminate == 0)
+  {
+    printf("This is the main loop, %d time.\n", loopCount);
     sleep(1);
     loopCount++;
   }
 
   printf("done.\n");
-  return(0);
-
-
-  
+  return (0);
 }
